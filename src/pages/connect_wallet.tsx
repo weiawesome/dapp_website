a2b70bdef0ef929f4d164c5639dc9b98f414d719
tcweeei <open891013@gmail.com>
2023-10-09 21:38:18 +0800

    Add the information get.

--- src/pages/connect_wallet.tsx
+++ src/pages/connect_wallet.tsx
@@ -3,7 +3,8 @@ import {ethers, formatEther, parseEther} from 'ethers';
 import Web3 from 'web3';
 import {useEffect, useState} from "react";
 import "../style/connect_wallet.css"
-export default function Home() {
+import {add} from "@noble/hashes/_u64";
+export default function Connect_wallet() {
     const [web3,setWeb3]=useState(null);
     const [web3Address,setWeb3Address]=useState("");
     const [web3Amount,setWeb3Amount]=useState("");
@@ -17,6 +18,28 @@ export default function Home() {
             setProvider(new ethers.BrowserProvider(window.ethereum));
         }
     },[])
+
+    const initWeb3=async () => {
+        if (typeof window.ethereum !== 'undefined') {
+            setWeb3Address("");
+            setWeb3Amount("");
+            await setWeb3(new Web3(window.ethereum));
+            try {
+                const accounts=await window.ethereum.request({method: 'eth_requestAccounts'});
+                const account = accounts[0];
+                const balanceWei = await web3.eth.getBalance(account);
+                const balanceEther = web3.utils.fromWei(balanceWei, 'ether');
+                setWeb3Amount(balanceEther);
+                setWeb3Address(accounts);
+                console.log('已連接到 MetaMask');
+            } catch (error) {
+                console.error('連接到 MetaMask 時出現錯誤：', error);
+            }
+        } else {
+            console.log('請安裝 MetaMask 錢包並連接到網絡');
+        }
+
+    }
     const initEthers=async ()=> {
         if (typeof window.ethereum !== 'undefined') {
             setEthersAddress("");
@@ -34,41 +57,19 @@ export default function Home() {
         }
     }
 
-    const initWeb3=async () => {
-        if (typeof window.ethereum !== 'undefined') {
-            setWeb3Address("");
-            setWeb3Amount("");
-            await setWeb3(new Web3(window.ethereum));
-            try {
-                const accounts=await window.ethereum.request({method: 'eth_requestAccounts'});
-                const account = accounts[0];
-                const balanceWei = await web3.eth.getBalance(account);
-                const balanceEther = web3.utils.fromWei(balanceWei, 'ether');
-                setWeb3Amount(balanceEther);
-                setWeb3Address(account);
-                console.log('已連接到 MetaMask');
-            } catch (error) {
-                console.error('連接到 MetaMask 時出現錯誤：', error);
-            }
-        } else {
-            console.log('請安裝 MetaMask 錢包並連接到網絡');
-        }
-
-    }
-
     return (
         <main className={"MainFrame"}>
             <h1 className={"Title"}>DApp 核心功能 之 連接錢包帳戶(MetaMask)</h1>
-            <div classname={"Content"}>
+            <div className={"Content"}>
                 <div className={"ConnectMethod"}>
-                    <button className={"ConnectBtn"} onClick={initEthers}>Connect to MetaMask by ethers.js</button>
-                    <h2 className={"ConnectAddr"}>帳戶地址: {ethersAddress}</h2>
-                    <h2 className={"ConnectAmount"}>帳戶餘額: {ethersAmount} ETH</h2>
-                </div>
-                <div className={"ConnectMethod"}>
-                    <button className={"ConnectBtn"} onClick={initWeb3}>Connect to MetaMask by web3.js</button>
+                    <button className={"ConnectBtn"} onClick={initWeb3}>Connect to MetaMask <br/>by web3.js</button>
                     <h2 className={"ConnectAddr"}>帳戶地址: {web3Address}</h2>
                     <h2 className={"ConnectAmount"}>帳戶餘額: {web3Amount} ETH</h2>
+                </div>
+                <div className={"ConnectMethod"}>
+                    <button className={"ConnectBtn"} onClick={initEthers}>Connect to MetaMask <br/>by ethers.js</button>
+                    <h2 className={"ConnectAddr"}>帳戶地址: {ethersAddress}</h2>
+                    <h2 className={"ConnectAmount"}>帳戶餘額: {ethersAmount} ETH</h2>
                 </div>
             </div>
         </main>
