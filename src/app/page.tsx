43f9a06483e5f3c70076f70eb0bee680f8941b69
tcweeei <open891013@gmail.com>
2023-10-11 16:21:52 +0800

    Add index page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,28 @@
 'use client';
-import {ethers, formatEther, parseEther} from 'ethers';
-import Web3 from 'web3';
-import {useEffect, useState} from "react";
-
+import "../style/index.css"
 export default function Home() {
     return (
         <main>
-            <h1>This is home page</h1>
+            <h1 className={"falling-text"}>
+                <span>
+                    D
+                </span>
+                <span>
+                    App
+                </span>
+                <span>
+                    &nbsp;&nbsp;&nbsp;&nbsp;(去中心化應用程序)
+                </span>
+            </h1>
+            <div>
+                <h2>甚麼是區塊鏈</h2>
+            </div>
+            <div>
+                <h2>甚麼是智能合約</h2>
+            </div>
+            <div>
+                <h2>甚麼是去中心化應用程序</h2>
+            </div>
         </main>
     )
 }
--- src/pages/build_transaction.tsx
+++ src/pages/build_transaction.tsx
@@ -3,7 +3,7 @@
 // import Web3 from 'web3';
 // import {useEffect, useState} from "react";
 // import "../style/connect_wallet.css"
-// export default function Connect_wallet() {
+// export default function Build_transaction() {
 //     const [web3,setWeb3]=useState(null);
 //     const [web3Address,setWeb3Address]=useState("");
 //     const [web3Amount,setWeb3Amount]=useState("");
@@ -56,17 +56,84 @@
 //         }
 //     }
 //
+//     const test=async () => {
+//         try {
+//             // 檢查 MetaMask 是否已安裝並連接
+//             if (!window.ethereum || !window.ethereum.isConnected()) {
+//                 throw new Error('請安裝 MetaMask 並連接到一個 Ethereum 網路');
+//             }
+//
+//             // 使用 MetaMask 的 provider 建立一個新的 ethers provider
+//             let provider = new ethers.BrowserProvider(window["ethereum"]);
+//
+//             // 取得已連接帳戶的 signer 物件
+//             const signer =await provider.getSigner();
+//
+//             const tx = {
+//                 to: '0x9FCfd3437f0a96A13D1Ef89bdd08b98034644b4b', // 替換成你想要發送到的地址
+//                 value: parseEther('10'),
+//             };
+//
+//             // 發送交易
+//             const transactionResponse = await signer.sendTransaction(tx);
+//
+//             console.log('交易已發送，等待確認...');
+//             console.log(transactionResponse);
+//
+//             // 等待交易被確認
+//             const transactionReceipt = await transactionResponse.wait();
+//
+//             console.log('交易已確認');
+//             console.log(transactionReceipt);
+//         } catch (error) {
+//             console.error('提交交易時發生錯誤:', error);
+//         }
+//     }
+//     const aa=async () => {
+//         if (!window.ethereum || !window.ethereum.isConnected()) {
+//             throw new Error('請安裝 MetaMask 並連接到一個 Ethereum 網路');
+//         }
+//         const provider = window.ethereum;
+//         const web3 = new Web3(provider);
+//         if (web3.eth.accounts.length === 0) {
+//             console.log('MetaMask 錢包未解鎖。');
+//             return;
+//         }
+//         const toAddress = '0x9FCfd3437f0a96A13D1Ef89bdd08b98034644b4b';
+//         const amountToSend = web3.utils.toWei('10', 'ether'); // 要轉送的以太數量
+//         const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+//         const account = accounts[0];
+//         console.log(amountToSend);
+//         const transactionObject = {
+//             from: account.toString(),
+//             to: toAddress,
+//             value: amountToSend,
+//         };
+//
+//         web3.eth.sendTransaction(transactionObject)
+//             .on('transactionHash', hash => {
+//                 console.log('交易哈希：', hash);
+//             })
+//             .on('receipt', receipt => {
+//                 console.log('交易收據：', receipt);
+//             })
+//             .on('error', error => {
+//                 console.error('發送交易時發生錯誤：', error);
+//             })
+//
+//     }
+//
 //     return (
 //         <main className={"MainFrame"}>
 //             <h1 className={"Title"}>DApp 核心功能 之 連接錢包帳戶(MetaMask)</h1>
 //             <div className={"Content"}>
 //                 <div className={"ConnectMethod"}>
-//                     <button className={"ConnectBtn"} onClick={initWeb3}>Connect to MetaMask <br/>by web3.js</button>
+//                     <button className={"ConnectBtn"} onClick={aa}>Connect to MetaMask <br/>by web3.js</button>
 //                     <h2 className={"ConnectAddr"}>帳戶地址: {web3Address}</h2>
 //                     <h2 className={"ConnectAmount"}>帳戶餘額: {web3Amount} ETH</h2>
 //                 </div>
 //                 <div className={"ConnectMethod"}>
-//                     <button className={"ConnectBtn"} onClick={initEthers}>Connect to MetaMask <br/>by ethers.js</button>
+//                     <button className={"ConnectBtn"} onClick={test}>Connect to MetaMask <br/>by ethers.js</button>
 //                     <h2 className={"ConnectAddr"}>帳戶地址: {ethersAddress}</h2>
 //                     <h2 className={"ConnectAmount"}>帳戶餘額: {ethersAmount} ETH</h2>
 //                 </div>
